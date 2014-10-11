5f42f823cbbc52de3bc57f7d6306bb01330a9c68
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2014-10-11 11:17:26 +0900

    add more comments

--- model.py
+++ model.py
@@ -6,59 +6,75 @@ Common data structure for model converter
 
 
 class ProjectModel(object):
-    name = None
-    body = None
+    """
+    Project model
+    """
+    name = None    #: Name of the simulation
+    bodies = []    #: List of body models
 
 
 class BodyModel(object):
-    name = None
+    """
+    Body model
+    """
+    name = None       #: Name of the body
+    links = []        #: List of links
+    trans = None      #: XYZ translation vector
+    rot = None        #: Rotation
 
 
 class LinkModel(object):
-    """Link model
-
-    Attributes:
-       inertial
-       visual
     """
-    inertial = None
-    visual = None
-    collision = None
-    trans = None
-    rot = None
+    Link model
+    """
+    inertial = None   #: Inertial (vector representation of 3x3 matrix)
+    visual = None     #: Shape information used for rendering
+    collision = None  #: Shape information used for collision detection
+    sensors = None    #: List of sensors
+    trans = None      #: XYZ translation vector
+    rot = None        #: Rotation
 
 
 class JointModel(object):
     """
     Joint model
     """
-    jointType = None
-    parent = None
-    child = None
-    damping = None
-    friction = None
-    limit = None
-    trans = None
-    rot = None
+    jointType = None  #: Joint type
+    parent = None     #: Parent link
+    child = None      #: Child link
+    damping = None    #: Damping
+    friction = None   #: Friction
+    limit = None      #: Joint limits (upper and lower in vector)
+    trans = None      #: XYZ translation vector
+    rot = None        #: Rotation
 
 
 class ShapeModel(object):
     """
     Shape model
     """
-    TYPE_MESH = 1
-    shapeType = None
+    SP_MESH = 1       #: Mesh shape
+    SP_BOX = 2        #: Box shape
+    SP_CYLINDER = 3   #: Cylinder shape
+    SP_CONE = 4       #: Cone shape
+    SP_SPHERE = 5     #: Sphere shape
+    SP_PLANE = 6      #: Plane shape
+
+    shapeType = None  #: Shape type
+    mesh = None       #: Mesh data (if the type is SP_MESH)
 
 
 class MeshModel(object):
-    vertex = None
-    normal = None
-    color = None
-    uvmap = None
-    image = None
+    vertex = []       #: Vertex position (in x,y,z * 3 * N format)
+    normal = []       #: Normal direction
+    color = []        #: Color (in RGBA * N format)
+    uvmap = []        #: UV mapping (in x,y * N format)
+    image = None      #: Texture image
+    trans = None      #: XYZ translation vector
+    rot = None        #: Rotation
 
 
 class SensorModel(object):
-    sensorType = None
-    trans = None
-    rot = None
+    sensorType = None  #: Type of sensor
+    trans = None       #: XYZ translation vector
+    rot = None         #: Rotation
--- sdf.py
+++ sdf.py
@@ -1,15 +1,19 @@
 import lxml
+import model
 
 
 class SDFReader:
     def read(self, f):
         d = lxml.etree.parse(open(f))
+        m = model.ProjectModel
 
         for w in d.findAll('world'):
             pass
 
         for m in d.findAll('model'):
             pass
+
+        return m
 
 
 class SDFWriter:
