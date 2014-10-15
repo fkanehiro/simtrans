3a52e821421f8ac258a025d3f9ae005c06b2714f
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2014-10-15 16:23:38 +0900

    implementing urdf loader

--- model.py
+++ model.py
@@ -9,63 +9,65 @@ class ProjectModel(object):
     """
     Project model
     """
-    name = None    #: Name of the simulation
-    bodies = []    #: List of body models
+    name = None        #: Name of the simulation
+    bodies = []        #: List of body models
 
 
 class BodyModel(object):
     """
     Body model
     """
-    name = None       #: Name of the body
-    links = []        #: List of links
-    trans = None      #: XYZ translation vector
-    rot = None        #: Rotation
+    name = None        #: Name of the body
+    links = []         #: List of links
+    joints = []        #: List of joints
+    trans = None       #: XYZ translation vector
+    rot = None         #: Rotation (quaternion representation)
 
 
 class LinkModel(object):
     """
     Link model
     """
-    name = None       #: Name of the link
-    mass = 0          #: Mass of the link
-    inertial = None   #: Inertial (vector representation of 3x3 matrix)
-    visual = None     #: Shape information used for rendering
-    collision = None  #: Shape information used for collision detection
-    sensors = None    #: List of sensors
-    trans = None      #: XYZ translation vector
-    rot = None        #: Rotation
+    name = None        #: Name of the link
+    mass = 0           #: Mass of the link
+    inertial = None    #: Inertial (vector representation of 3x3 matrix)
+    visual = None      #: Shape information used for rendering
+    collision = None   #: Shape information used for collision detection
+    sensors = None     #: List of sensors
+    trans = None       #: XYZ translation vector
+    rot = None         #: Rotation (quaternion representation)
 
 
 class JointModel(object):
     """
     Joint model
     """
-    jointType = None  #: Joint type
-    parent = None     #: Parent link
-    child = None      #: Child link
-    damping = None    #: Damping
-    friction = None   #: Friction
-    limit = None      #: Joint limits (upper and lower in vector)
-    trans = None      #: XYZ translation vector
-    rot = None        #: Rotation
+    jointType = None   #: Joint type
+    axis = None        #: Joint axis
+    parent = None      #: Parent link
+    child = None       #: Child link
+    damping = None     #: Damping
+    friction = None    #: Friction
+    limit = None       #: Joint limits (upper and lower in vector)
+    trans = None       #: XYZ translation vector
+    rot = None         #: Rotation (quaternion representation)
 
 
 class ShapeModel(object):
     """
     Shape model
     """
-    SP_MESH = 1       #: Mesh shape
-    SP_BOX = 2        #: Box shape
-    SP_CYLINDER = 3   #: Cylinder shape
-    SP_CONE = 4       #: Cone shape
-    SP_SPHERE = 5     #: Sphere shape
-    SP_PLANE = 6      #: Plane shape
+    SP_MESH = 1        #: Mesh shape
+    SP_BOX = 2         #: Box shape
+    SP_CYLINDER = 3    #: Cylinder shape
+    SP_CONE = 4        #: Cone shape
+    SP_SPHERE = 5      #: Sphere shape
+    SP_PLANE = 6       #: Plane shape
 
-    shapeType = None  #: Shape type
-    mesh = None       #: Mesh data (if the type is SP_MESH)
-    trans = None      #: XYZ translation vector
-    rot = None        #: Rotation
+    shapeType = None   #: Shape type
+    mesh = None        #: Mesh data (if the type is SP_MESH)
+    trans = None       #: XYZ translation vector
+    rot = None         #: Rotation (quaternion representation)
 
 
 class MeshModel(object):
@@ -78,7 +80,7 @@ class MeshModel(object):
     uvmap = []        #: UV mapping (in x,y * N format)
     image = None      #: Texture image
     trans = None      #: XYZ translation vector
-    rot = None        #: Rotation
+    rot = None        #: Rotation (quaternion representation)
 
 
 class SensorModel(object):
@@ -87,4 +89,4 @@ class SensorModel(object):
     """
     sensorType = None  #: Type of sensor
     trans = None       #: XYZ translation vector
-    rot = None         #: Rotation
+    rot = None         #: Rotation (quaternion representation)
--- urdf.py
+++ urdf.py
@@ -6,6 +6,7 @@ Reader and writer for URDF format
 
 import lxml
 import numpy
+import jinja2
 import model
 import collada
 import stl
@@ -19,33 +20,68 @@ class URDFReader:
         for l in d.findAll('link'):
             lm = model.LinkModel
             lm.name = l.attrib['name']
-            inertial = l.find('inertial')
-            lm.inertia = self.readInertia(inertial.find('inertial'))
-            lm.trans = self.readOrigin(inertial.find('origin'))
-            lm.mass = self.readMass(inertial.find('mass'))
-            lm.visual = self.readVisual(l.find('visual'))
-            lm.collision = self.readCollision(l.find('collision'))
+            with l.find('inertial') as inertial:
+                lm.inertia = self.readInertia(inertial.find('inertia'))
+                lm.trans = self.readOrigin(inertial.find('origin'))
+                lm.mass = self.readMass(inertial.find('mass'))
+            with l.find('visual') as visual:
+                sm = model.ShapeModel
+                sm.trans = self.readOrigin(visual.find('origin'))
+                with visual.find('geometry') as geometry:
+                    with geometry.find('mesh') as mesh:
+                        sm.shapeType = m.SP_MESH
+                        reader = collada.ColladaReader
+                        sm.mesh = reader.read(self.resolveFile(mesh.attrib['filename']))
+                lm.visual = sm
+            with l.find('collision') as collision:
+                sm = model.ShapeModel
+                sm.trans = self.readOrigin(collision.find('origin'))
+                with collision.find('geometry') as geometry:
+                    with geometry.find('mesh') as mesh:
+                        sm.shapeType = m.SP_MESH
+                        reader = stl.STLReader
+                        sm.mesh = reader.read(self.resolveFile(mesh.attrib['filename']))
+                lm.collision = sm
+            bm.links.append(lm)
 
         for j in d.findAll('joint'):
             jm = model.JointModel
             jm.origin = self.readOrigin(j.find('origin'))
+            jm.axis = j.find('axis').attrib['xyz']
+            jm.parent = j.find('parent').attrib['link']
+            jm.child = j.find('child').attrib['link']
+            with j.find('dynamics') as dynamics:
+                jm.damping = dynamics.attrib['damping']
+                jm.friction = dynamics.attrib['friction']
+            with j.find('limit') as limit:
+                jm.limit = [limit.attrib['upper'], limit.attrib['lower']]
+            bm.joints.append(jm)
+
+        return bm
 
     def readOrigin(self, d):
-        xyz = d.attrib['xyz'].split(' ')
-        rpy = d.attrib['rpy'].split(' ')
+        xyz = numpy.matrix(d.attrib['xyz'])
+        rpy = numpy.matrix(d.attrib['rpy'])
 
     def readInertia(self, d):
-        inertia = numpy.matrix
-        inertia = d.attrib['ixx']
+        inertia = numpy.zeros((3,3))
+        inertia[0,0] = float(d.attrib['ixx'])
+        inertia[0,1] = inertia[1,0] = float(d.attrib['ixy'])
+        inertia[0,2] = inertia[2,0] = float(d.attrib['ixz'])
+        inertia[1,1] = float(d.attrib['iyy'])
+        inertia[1,2] = inertia[2,1] = float(d.attrib['iyz'])
+        inertia[2,2] = float(d.attrib['izz'])
 
     def readMass(self, d):
         return float(d.attrib['value'])
 
-    def readVisual(self, d):
-        m = model.ShapeModel
-        m.shapeType = m.SP_MESH
-        reader = collada.ColladaReader
-        m.mesh = reader.read(d.find('geometry').find('mesh').attrib['filename'])
+    def resolveFile(self, f):
+        if f.count('package://') > 0:
+            pkgname = f.lstrip('package://').split('/')[0]
+            ppath = subprocess.check_output(['rospack', 'find', pkgname]).rstrip()
+            return os.path.join(ppath, f.lstrip('package://'))
+        return f
+
 
 class URDFWriter:
     def write(self, m, f):
