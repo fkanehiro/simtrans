1be9d8b66562fc53fbf82e98b81217149703441f
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2014-10-28 10:56:48 +0900

    fix recursion structure

--- simtrans/collada.py
+++ simtrans/collada.py
@@ -38,48 +38,48 @@ class ColladaReader(object):
             self._materials[m.id] = m
         m = model.ShapeModel()
         m.shapeType = model.ShapeModel.SP_NONE
+        m.children = []
         for n in d.scene.nodes:
-            m.children.extend(self.convertchildren(n))
+            m.children.append(self.convertchild(n))
         return m
 
-    def convertchildren(self, d):
-        nodes = []
-        for c in d.children:
-            if type(c) == collada.scene.Node:
-                m = model.ShapeModel()
-                m.shapeType = model.ShapeModel.SP_NONE
-                dmat = tf.decompose_matrix(c.matrix)
-                m.scale = dmat[0].tolist()
-                m.trans = dmat[3].tolist()
-                m.rot = tf.quaternion_from_matrix(tf.compose_matrix(shear=dmat[1], angles=dmat[2]))
-                m.children = self.convertchildren(c)
-                nodes.append(m)
-            elif type(c) == collada.scene.GeometryNode:
-                m = model.ShapeModel()
-                m.shapeType = model.ShapeModel.SP_MESH
-                for p in c.geometry.primitives:
-                    gm = model.MeshModel()
-                    gm.vertex = p.vertex
-                    gm.vertex_index = p.vertex_index
-                    if p.normal.size > 0:
-                        gm.normal = p.normal
-                        gm.normal_index = p.normal_index
-                    if len(p.texcoordset) > 0:
-                        gm.uvmap = p.texcoordset[0]
-                        gm.uvmap_index = p.texcoord_indexset[0]
-                    m.children.append(gm)
-                    try:
-                        for pr in self._materials[p.material].effect.params:
-                            if type(pr) == collada.material.Surface:
-                                fname = os.path.abspath(os.path.join(self._basepath, pr.image.path))
-                                if self._assethandler:
-                                    m.image = self._assethandler(fname)
-                                else:
-                                    m.image = fname
-                    except KeyError:
-                        pass
-                nodes.append(m)
-        return nodes
+    def convertchild(self, d):
+        m = None
+        if type(d) == collada.scene.Node:
+            m = model.ShapeModel()
+            m.shapeType = model.ShapeModel.SP_NONE
+            dmat = tf.decompose_matrix(d.matrix)
+            m.scale = dmat[0].tolist()
+            m.trans = dmat[3].tolist()
+            m.rot = tf.quaternion_from_matrix(tf.compose_matrix(shear=dmat[1], angles=dmat[2]))
+            m.children = []
+            for c in d.children:
+                m.children.append(self.convertchild(c))
+        elif type(d) == collada.scene.GeometryNode:
+            m = model.ShapeModel()
+            m.shapeType = model.ShapeModel.SP_MESH
+            for p in d.geometry.primitives:
+                gm = model.MeshModel()
+                gm.vertex = p.vertex
+                gm.vertex_index = p.vertex_index
+                if p.normal.size > 0:
+                    gm.normal = p.normal
+                    gm.normal_index = p.normal_index
+                if len(p.texcoordset) > 0:
+                    gm.uvmap = p.texcoordset[0]
+                    gm.uvmap_index = p.texcoord_indexset[0]
+                m.children.append(gm)
+                try:
+                    for pr in self._materials[p.material].effect.params:
+                        if type(pr) == collada.material.Surface:
+                            fname = os.path.abspath(os.path.join(self._basepath, pr.image.path))
+                            if self._assethandler:
+                                m.image = self._assethandler(fname)
+                            else:
+                                m.image = fname
+                except KeyError:
+                    pass
+        return m
 
 
 class ColladaWriter(object):
