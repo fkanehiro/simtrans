6a5f399da5a215f59e60f41d2a0fbae758857d5a
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2014-11-10 17:02:57 +0900

    file resolver now support gazebo's model cache

--- simtrans/utils.py
+++ simtrans/utils.py
@@ -18,19 +18,21 @@ def resolveFile(f):
     '/opt/ros/indigo/share/atlas_description/package.xml'
     >>> resolveFile('package://atlas_description/urdf/atlas.urdf')
     '/opt/ros/indigo/share/atlas_description/urdf/atlas.urdf'
-    >>> resolveFile('model://pr2/model.sdf')
-    '/usr/share/gazebo-4.0/models/pr2/model.sdf'
-    >>> resolveFile('model://PA10/pa10.main.wrl')
-    '/usr/local/share/OpenHRP-3.1/sample/model/PA10/pa10.main.wrl'
+    >>> resolveFile('model://pr2/model.sdf') == os.path.expanduser('~/.gazebo/models/pr2/model.sdf')
+    True
+    >>> resolveFile('model://PA10/pa10.main.wrl') == os.path.expandvars('$OPENHRP_MODEL_PATH/PA10/pa10.main.wrl')
+    True
     '''
     try:
         if f.count('model://') > 0:
             fn = f.replace('model://', '')
+            paths = ['~/.gazebo/models']
             for env in ['GAZEBO_MODEL_PATH', 'OPENHRP_MODEL_PATH']:
-                for p in os.environ[env].split(':'):
-                    ff = os.path.join(p, fn)
-                    if os.path.exists(ff):
-                        return ff
+                paths.extend(os.environ[env].split(':'))
+            for p in paths:
+                ff = os.path.expanduser(os.path.join(p, fn))
+                if os.path.exists(ff):
+                    return ff
         if f.count('package://') > 0:
             pkgname, pkgfile = f.replace('package://', '').split('/', 1)
             ppath = subprocess.check_output(['rospack', 'find', pkgname]).rstrip()
