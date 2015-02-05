23cddfb4b18cd3fb1557dda125d82f1a90835027
Fumio KANEHIRO <fkanehiro@gmail.com>
2015-02-05 22:56:30 +0900

    adds python-omniorb

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,7 @@
 set -x
 
 sudo apt-get update -qq
-sudo apt-get install -qq -y python-pip graphviz
+sudo apt-get install -qq -y python-pip graphviz python-omniorb
 sudo pip install -r requirements-dev.txt
 
 cd doc
