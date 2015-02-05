7fbb9fe9547cf8c045bdae68b5b8e07183ebeb12
Fumio KANEHIRO <fkanehiro@gmail.com>
2015-02-05 22:21:27 +0900

    fixes a filename

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ set -x
 
 sudo apt-get update -qq
 sudo apt-get install -qq -y python-pip graphviz
-sudo pip install -r requires.txt
+sudo pip install -r requirements.txt
 
 cd doc
 source ./build.sh
