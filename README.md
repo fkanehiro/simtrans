11b741d06edd2c53c35285b1e931aaca8ac3122c
Fumio KANEHIRO <fkanehiro@gmail.com>
2015-02-05 22:16:21 +0900

    removes unused files

--- README.md
+++
@@ -1,1 +0,0 @@
-[![Build Status](https://travis-ci.org/fkanehiro/simtrans.svg?branch=master)](https://travis-ci.org/fkanehiro/simtrans)
--- env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export OPENHRP_MODEL_PATH=/usr/local/share/OpenHRP-3.1/sample/model
-export ORBgiopMaxMsgSize=2097152000
-. /opt/ros/indigo/setup.sh
-. /usr/share/drcsim/setup.sh
-#export GAZEBO_MODEL_PATH=/tmp:$GAZEBO_MODEL_PATH
