12935586b39f9bbf71f102f3fcb458504c34ea06
Fumio KANEHIRO <fkanehiro@gmail.com>
2015-02-05 22:34:42 +0900

    uses requirements-dev.txt instead of requirements.txt

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ set -x
 
 sudo apt-get update -qq
 sudo apt-get install -qq -y python-pip graphviz
-sudo pip install -r requirements.txt
+sudo pip install -r requirements-dev.txt
 
 cd doc
 source ./build.sh
