910ad2f8c57b7b530df9433987f4f27c9fbac52e
Fumio KANEHIRO <f-kanehiro@aist.go.jp>
2015-02-06 12:35:33 +0900

    adds omniorb packages to dependency list

--- .travis.sh
+++ .travis.sh
@@ -5,7 +5,7 @@ set -x
 
 sudo add-apt-repository -y ppa:hrg/daily
 sudo apt-get update -qq
-sudo apt-get install -qq -y python-pip graphviz openhrp openrtm-aist-python python-omniorb
+sudo apt-get install -qq -y python-pip graphviz openhrp openrtm-aist-python python-omniorb omniidl-python omniorb-idl omniidl
 sudo pip install -r requirements-dev.txt
 
 cd doc
