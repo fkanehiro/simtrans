f89257a62f29546480bd5fcedca22d46cdb61bd9
Fumio KANEHIRO <f-kanehiro@aist.go.jp>
2015-02-06 09:35:18 +0900

    fixes a package name

--- .travis.sh
+++ .travis.sh
@@ -5,7 +5,7 @@ set -x
 
 sudo add-apt-repository -y ppa:hrg/daily
 sudo apt-get update -qq
-sudo apt-get install -qq -y python-pip graphviz openhrp3
+sudo apt-get install -qq -y python-pip graphviz openhrp
 sudo pip install -r requirements-dev.txt
 
 cd doc
