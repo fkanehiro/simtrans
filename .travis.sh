c3ff171d63abd2c1a7295d758388879d0e9d1b6a
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2015-11-06 11:40:43 +0900

    adjust travis

--- .travis.sh
+++ .travis.sh
@@ -16,7 +16,7 @@ sudo add-apt-repository -y ppa:hrg/daily
 
 # install required packages
 sudo apt-get update -qq
-sudo apt-get install -qq -y python-pip graphviz openhrp openrtm-aist-python python-omniorb omniidl-python omniorb-idl omniidl python-numpy ros-$ROS_DISTRO-xacro ros-$ROS_DISTRO-pr2-description ros-$ROS_DISTRO-ur-description ros-$ROS_DISTRO-baxter-description drcsim
+sudo apt-get install -qq -y python-pip graphviz meshlab openhrp openrtm-aist-python python-omniorb omniidl-python omniorb-idl omniidl python-numpy ros-$ROS_DISTRO-xacro ros-$ROS_DISTRO-pr2-description ros-$ROS_DISTRO-ur-description ros-$ROS_DISTRO-baxter-description drcsim
 
 # install python libraries
 sudo pip install --upgrade pip
